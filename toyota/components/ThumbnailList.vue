9c88076e9572be1c82e72a579d924a188fb90d4c
gotaku6629 <gotakugto510@gmail.com>
2021-10-04 10:42:58 +0900

    firebaseとの連携

---
+++ toyota/components/ThumbnailList.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-card class="d-flex justify-space-around flex-wrap" style="min-height: 90vh">
+    <Thumbnail v-for="movie in movies" :key="movie.id" :movie-id="movie.id" />
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    category: {
+      type: String,
+      default: '',
+      required: false,
+    },
+  },
+  computed: {
+    movies() {
+      return this.$store.getters['movies/byCategory'](this.category)
+    },
+  },
+}
+</script>
---
+++ toyota/store/rooms.js
@@ -0,0 +1,83 @@
+import { firestoreAction } from 'vuexfire'
+
+export const state = () => ({
+  items: []
+})
+
+export const getters = {
+  all: state => {
+    return state.items
+  },
+  byId: (state) => (roomId) => {
+    const res = state.items.filter(room => room.id === roomId)
+    return res.length > 0 ? res[0] : null
+  },
+  byMovie: (state) => (movieId) => {
+    return state.items.filter(room => room.movie_uid === movieId)
+  },
+  countAudience: (state) => (movieId) => {
+    const rooms = state.items.filter(room => room.movie_uid === movieId)
+    let count = 0
+    for (const room of rooms) {
+      count += room.users.length
+    }
+
+    return count
+  },
+}
+
+export const actions = {
+  bind: firestoreAction(function ({ bindFirestoreRef }) {
+    // return the promise returned by `bindFirestoreRef`
+    return bindFirestoreRef('items', this.$fire.firestore.collection('rooms'))
+  }),
+  add: firestoreAction(function (_, { title, users, detail, adminId, skyway, movieId, id }) {
+    return this.$fire.firestore.collection('rooms').add({ title, users, detail, admin_uid: adminId, skyway, movie_uid: movieId, id })
+  }),
+  remove: firestoreAction(function ({ getters }, { roomId }) {
+    const user = this.$fire.auth().currentUser;
+
+    if (user === null) {
+      throw new Error('is not logged in')
+    }
+
+    const room = getters.byId(roomId);
+    if (!room) {
+      throw new Error('invalid room id')
+    }
+
+    if (room.admin_uid !== user.uid) {
+      throw new Error('permission denied')
+    }
+
+    return this.$fire.firestore.collection('rooms').doc(roomId).delete()
+  }),
+  join: firestoreAction(function ({ getters }, { roomId }) {
+    const user = this.$fire.auth.currentUser;
+
+    if (user === null) {
+      throw new Error('is not logged in')
+    }
+
+    const room = getters.byId(roomId)
+    if (!room) {
+      throw new Error('invalid room id')
+    }
+
+    return this.$fire.firestore.collection('rooms').doc(roomId).update({ users: this.$fireModule.firestore.FieldValue.arrayUnion(user.uid) })
+  }),
+  leave: firestoreAction(function ({ getters }, { roomId }) {
+    const user = this.$fire.auth.currentUser;
+
+    if (user === null) {
+      throw new Error('is not logged in')
+    }
+
+    const room = getters.byId(roomId)
+    if (!room) {
+      throw new Error('invalid room id')
+    }
+
+    return this.$fire.firestore.collection('rooms').doc(roomId).update({ users: this.$fireModule.firestore.FieldValue.arrayRemove(user.uid) })
+  }),
+}
